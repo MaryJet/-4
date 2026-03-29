a2c76331e1677054fd1a2aafaf359402a13ba1b5
MaryJet <marikmarikmarik212@gmail.com>
2026-03-29 19:14:20 +0300

    Add files via upload

---
+++ extra_module.py
@@ -0,0 +1,9 @@
+
+# Дополнительный модуль для командной разработки
+def find_max(numbers):
+    """Находит максимум в списке"""
+    return max(numbers) if numbers else None
+
+def find_min(numbers):
+    """Находит минимум в списке"""
+    return min(numbers) if numbers else None
